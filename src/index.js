4889c79cfa4e8b72abd2a508319b19fa655b276d
Wagner Goulart <wcfx.dev@gmail.com>
2021-09-12 04:28:30 -0300

    feat: if user already exists dont register

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
+app.use(express.json());
 
-app.listen(3333, console.log('Olá Marilene'));
+const customers = [];
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf);
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ message: 'Customer already exists'})
+  }
+
+  customers.push({
+    id: uuidv4(),
+    cpf,
+    name,
+    statement: []
+  });
+
+  return res.status(201).send();
+});
+
+
+app.listen(3333);
